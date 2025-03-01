5ac1d956dd193c3675a15ad49c3a37614f4b3340
SamedHrmn <samed_harman@hotmail.com>
2025-03-01 12:41:11 +0300

    face restoration backend completed

--- photo-enhancer-service/functions/src/index.ts
+++ photo-enhancer-service/functions/src/index.ts
@@ -1,9 +1,9 @@
 import { verifyIntegrity } from "./verify_integrity";
 import { createUser, getUserData, deleteAccount, updateUserCredit } from "./user_manager";
-import { colorizeImage, deblurImage } from "./image_enhancer";
+import { colorizeImage, deblurImage, faceRestoration } from "./image_enhancer";
 import { verifyPurchase } from "./purchase_manager";
 
 export { verifyIntegrity };
 export { createUser, getUserData, deleteAccount, updateUserCredit };
-export { colorizeImage, deblurImage };
+export { colorizeImage, deblurImage, faceRestoration };
 export { verifyPurchase };
